f0e135378e403d6e725e74232b1d6f9f8a172e54
BoBkiNN_ <78136575+BoBkiNN@users.noreply.github.com>
2025-01-14 04:06:11 +0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -3,6 +3,8 @@ const { mainAction } = require('./metadata_parser');
 async function run() {
     const d = await mainAction("https://repo.maven.apache.org/maven2", "org.apache.maven.plugins.maven-jar-plugin")
     console.log(d)
+    const lv = d.latest_version
+    console.log(lv+":"+typeof(lv))
 }
 
 run()
