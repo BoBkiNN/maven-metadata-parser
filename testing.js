c8fe996ff0ea4b3deb3214a8d9bcf8c26ff86213
BoBkiNN <bobkin.yangirov@gmail.com>
2025-01-14 02:56:20 +0500

    change testing

--- testing.js
+++ testing.js
@@ -1,7 +1,7 @@
 const { mainAction } = require('./metadata_parser');
 
 async function run() {
-    const d = await mainAction("https://repo.papermc.io/repository/maven-snapshots", "io.papermc.paper.paper-api")
+    const d = await mainAction("https://repo.maven.apache.org/maven2", "org.apache.maven.plugins.maven-jar-plugin")
     console.log(d)
 }
 
